3f38ca0cb4a59bba2bb4841cbd074dc1e249a1a8
Albertas Gimbutas <albertasgim@gmail.com>
2010-02-21 00:48:33 +0200

    Užbaigta webstat programa

--- webstat.py
+++ webstat.py
@@ -5,21 +5,25 @@
 1. Kokiu greičių parsiųstas puslapis (bps)
 2. Puslapio turinio SHA1 kontrolinę sumą
 3. Puslapio turinio baitų skaičių
-4. Puslapio eilučių skaičių (<br> žymių skaičius) 
+4. Puslapio eilučių skaičių (<br> arba <p> žymių skaičius) 
 
-Realizuotos dvi funkcijos: testFunciton() bei getWebStat(url), pavyzdžiui:
-
->>> testFunction()
-4
+Programoje realizuotas metodas getWebStat() bei clasė MyHTMLParser
 """
 import sys
 import time
 import urllib
+import hashlib
+import HTMLParser
 
-def testFunction():
-    return 4
+class MyHTMLParser(HTMLParser.HTMLParser):
+    """ Parsinimo klasė skirta suskaičiuoti dokumento naujų eilučių žymes """
+    newlines = 0
+    def handle_starttag(self, tag, attrs):
+        if tag in ['p', 'br']:
+            self.newlines += 1
 
-def main():
+def getWebStat():
+    """ Raportuok puslapio statistikas """
     if len(sys.argv) < 2:
         url = raw_input("Įveskite puslapio adresą: ")
     else:
@@ -27,11 +31,18 @@ def main():
     time_before = time.time()
     f = urllib.urlopen(url)
     time_after = time.time()
-    byte_count = len(f.read())
-    load_speed = byte_count * 8 / (time_after - time_before)
-    print "Puslapio parsiuntimo greitis:", load_speed, "bps."
+    page_content = f.read()
+    f.close()
+    load_speed = len(page_content) * 8 / (time_after - time_before)
+    print "Puslapio parsiuntimo greitis:", load_speed, "bps"
+    print "Puslapio SHA1 kontrolinė suma:"
+    print hashlib.sha1(page_content).hexdigest()
+    print "Puslapio baitų skaičius:", len(page_content)
+    myparser = MyHTMLParser()
+    myparser.feed(page_content)
+    print "Puslapyje yra", myparser.newlines, "naujų eilučių"
     
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-    main()
+    getWebStat()
